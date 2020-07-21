4e1785873ab177f281e0e5b2dae8e66af08bfec2
Charlie Phillips <cgp246@gmail.com>
2020-07-20 20:41:14 -0400

    Switch to new algo & method

--- receive.py
+++ receive.py
@@ -1,51 +1,23 @@
 from config import *
+import numpy as np
+from scipy.io import wavfile
+from scipy.signal import iirfilter
+import matplotlib.pyplot as plt
 
-
-def detect_hamming_error(arr, nr):
-    n = len(arr)
-    res = -1
-
-    # Calculate parity bits again
-    for i in range(nr):
-        val = 0
-        for j in range(1, n + 1):
-            if (j & (2 ** i) == (2 ** i)):
-                val = val ^ int(arr[-1 * j])
-
-            # Create a binary no by appending
-        # parity bits together.
-
-        res = res + val * (10 ** i)
-
-    # Convert binary to decimal
-    return int(str(res), 2)
-
-def detect_parity_error(data):
-    parity = int(data[0])
-    par_data = data[1::]
-    int_data = list(map(int, par_data))
-    count = sum(int_data)
-    par = count % 2
-    if par != parity:
-        # return None
-        print("Parity error")
-        return par_data
-    else:
-        return par_data
-
+F_0_RAD = (2 * np.pi) /  F_0 
+F_1_RAD = (2 * np.pi) /  F_1 
 
 if __name__ == "__main__":
-    a = '0101010100101101'
-    unpar_data = detect_parity_error(a)
-    if unpar_data:
-        error = detect_hamming_error(unpar_data, 4)
-        if error == -1:
-            print("Recv:", unpar_data)
-        else:
-            unpar_data = list(unpar_data)
-            unpar_data[error] = '1' if unpar_data[error] == '0' else '0'
-            print("Recv:", ''.join(unpar_data))
+    fs, audio_input = wavfile.read('asdf.wav')
+    print(audio_input.shape)
+    plt.plot(audio_input)
+    plt.show()
+    goertzel_intermediate = np.zeros(audio_input.shape[0])
+    precomp = 2 * np.cos(F_0_RAD)
+    for i in range(audio_input.shape[0]):
+        goertzel_intermediate[i] = audio_input[i] + precomp * (goertzel_intermediate[i-1] if i > 1 else 0) - (goertzel_intermediate[i-2] if i > 2 else 0)
+    goertzel_output = np.zeros(audio_input.shape[0])
+    for i in range(audio_input.shape[0]):
+        goertzel_output[i] = goertzel_intermediate[i] - np.exp
 
-    else:
-        print("Parity Error!")
-
+    
